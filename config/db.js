289246cb141a6cd89a2b4e1b85c704cda2b474e2
LitBowya <agbagabenjamin819@gmail.com>
2025-03-13 07:14:22 +0000

    commits

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
+    console.log("⏳ Connecting to MongoDB..."); // Log before connecting
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
