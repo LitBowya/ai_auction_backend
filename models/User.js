bd882a81e27f8b514b23bcce66664438458b97f1
LitBowya <agbagabenjamin819@gmail.com>
2025-03-25 14:48:53 +0000

    commits

--- models/User.js
+++ models/User.js
@@ -6,7 +6,7 @@ const UserSchema = new mongoose.Schema(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    phone: { type: String, required: true, unique: true },
+    phone: { type: String, required: true},
     profileImage: { type: String },
     address: { type: String, required: true },
     verified: { type: Boolean, default: false },
